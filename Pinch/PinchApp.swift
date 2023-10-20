27879a8b071896e3ca0d71b9eb7854aecedcd261
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-10-20 11:49:25 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Lexter Tapawan on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
